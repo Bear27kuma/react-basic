285fc382c478066d19c5f30e9114adf51a86b18e
Yuta Okuma <y.dream.nest@icloud.com>
2022-04-13 19:53:23 +0900

    ショートサーキット評価

---
+++ 02_javascript/05_operator/short-circuit.js
@@ -0,0 +1,8 @@
+// ショートサーキット評価（短絡評価）
+const hello = undefined || null || 0 || NaN || '' || 'Hello';
+const chao = ' ' && 100 && [] && {} && 'Chao!';
+
+true && console.log('1.', hello);
+false && console.log('2.', hello);
+true || console.log('3.', chao);
+false || console.log('4.', chao);
