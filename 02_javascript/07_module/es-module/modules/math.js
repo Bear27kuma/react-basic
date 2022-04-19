ed8c3531ec16c27bab2fab2b62d7fdad5f56fb33
Yuta Okuma <y.dream.nest@icloud.com>
2022-04-19 09:33:03 +0900

    デフォルトエクスポートと名前付きエクスポート

--- 02_javascript/07_module/es-module/modules/math.js
+++ 02_javascript/07_module/es-module/modules/math.js
@@ -1,5 +1,5 @@
 // デフォルトエクスポート（Default Export）
-import { ONE, TWO as ZWEI } from './constants';
+import { ONE, TWO as ZWEI } from './constants.js';
 
 export const plus = (n, m = ONE) => n + m;
 const times = (n, m = ZWEI) => n * m;
