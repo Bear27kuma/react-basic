ed400a506bb7482323bcd3eea3e54a0488bbb82e
Yuta Okuma <y.dream.nest@icloud.com>
2022-04-21 09:23:32 +0900

    手続型プログラミングと関数型プログラミング

---
+++ 03_functional_programming/01_advantages/comparison.js
@@ -0,0 +1,20 @@
+// Procedural programming（手続型プログラミング）
+{
+  const octuples = [];
+
+  for (let n = 1; n < 101; n += 1) {
+    if (n % 8 === 0) {
+      octuples.push(n);
+    }
+  }
+
+  console.log(octuples);
+}
+
+// Functional programming（関数型プログラミング）
+{
+  const range = (start, end) => [...new Array(end - start).keys()].map(
+    (n) => n + start);
+  console.log(range(1, 101).filter((n) => n % 8 === 0));
+}
+
