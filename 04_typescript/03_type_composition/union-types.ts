f0cc1d624f103602e65fdf832ae790b764c2f713
Yuta Okuma <y.dream.nest@icloud.com>
2022-05-10 23:47:29 +0900

    共用体型（ユニオン型）

---
+++ 04_typescript/03_type_composition/union-types.ts
@@ -0,0 +1,13 @@
+type A = {
+  foo: number;
+  bar?: string;
+};
+
+type B = { foo: string };
+type C = { bar: string };
+type D = { baz: boolean };
+
+// ユニオン型
+type AorB = A | B;  // { foo: number | string; bar?: string }
+type AorC = A | C;  // { foo: number; bar?: string } or { bar: string }
+type AorD = A | D;  // { foo: number; bar?: string } or { baz: boolean }
